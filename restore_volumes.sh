08641b10cf784ba6aa0519c4bc6fff59edd9ebd6
Ilkka Tengvall <itengval@redhat.com>
2025-05-10 09:13:13 +0300

    Add restore for volume dirs

--- restore_volumes.sh
+++ restore_volumes.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 
-[ "$(ls -A $HOME/storage)" ] && tar xzf /var/tmp/s2i-volume-save.tgz -C $HOME storage 
-[ "$(ls -A $HOME/public/assets)" ] && tar xzf /var/tmp/s2i-volume-save.tgz -C $HOME public/assets
+[ -z "$(ls -A $HOME/storage)" ] && \
+  tar xzvf /var/tmp/s2i-volume-save.tgz -C $HOME 'storage/*' || \
+  echo "Storage not empty, skip restore"
+[ -z "$(ls -A $HOME/public/assets)" ] && \
+  tar xzvf /var/tmp/s2i-volume-save.tgz -C $HOME 'public/assets/*' || \
+  echo  "Assets not empty, skip restore"
 
+echo "You can find build time files for volumes in /var/tmp/s2i-volume-save.tgz."
+echo "E.g. after installing new plugins or update, you might want to apply"
+echo "those to run time using tar xzvf /var/tmp/s2i-volume-save.tgz -C $HOME."
